9629c71562f23ddefacd291ae49998c307908534
lsxzhq <96367896+lsxzhq@users.noreply.github.com>
2022-02-04 16:04:56 +0800

    Update augmentations.py

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -23,11 +23,10 @@ class Albumentations:
 
             self.transform = A.Compose([
                 A.Blur(p=0.01),
-                A.MedianBlur(p=0.3),
-                A.MotionBlur(p=0.3),
+                A.MedianBlur(p=0.01),
                 A.ToGray(p=0.01),
-                A.CLAHE(p=0.3),
-                A.RandomBrightnessContrast(p=0.3),
+                A.CLAHE(p=0.01),
+                A.RandomBrightnessContrast(p=0.0),
                 A.RandomGamma(p=0.0),
                 A.ImageCompression(quality_lower=75, p=0.0)],
                 bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
